215f71f82110f4ad6da6c0f58253bd800ce4035f
Richard Edmund Florendo <richard.florendo24@gmail.com>
2023-08-08 16:21:28 +0400

    Uploaded Day 1 of Python Programming Training
    Uploaded the Last Files in Day 1 of Python Programming Training in Time Training Center

---
+++ TrainingDay1/print.py
@@ -0,0 +1,72 @@
+'''print("hellloooo")
+print(4)
+print(4.5)'''
+
+#variable
+
+#age=20
+#name="Richard"
+'''print(age)
+print(name)
+
+print("hi",name,"your age is:",age)
+print(f"Hi,{name} you are {age}years old")'''
+
+#Creating variables
+
+'''print("Testing")
+
+test = "This is a test"
+sample = 8
+
+print(f"This message is a sample. {test}, the number in the variable is {sample}")
+print("This message is a sample", test, ", the number in the variable is", sample)'''
+
+'''print("I will calculate your age in some years")
+name=input("Enter your Name:")
+#print(name)
+
+age = int(input("Enter your age:"))
+
+future = int(input("Enter the future year:"))
+
+answer = (future - 2023) + age
+
+print(f"Hello {name}, you are now {age} years old, in the year {future} you will be {answer} years old")'''
+
+'''print("Input 2 numbers and this program will output those 2 values and their operations with each other")
+firstNumber = int(input("Input the first number: "))
+secondNumber = int(input("Input the second number : "))
+
+print("Here are the answers:")
+print("Addition: ", firstNumber, "+", secondNumber, "=", firstNumber + secondNumber)
+print("Subtraction: ", firstNumber, "-", secondNumber, "=", firstNumber - secondNumber)
+print("Multiplication: ", firstNumber, "*", secondNumber, "=", firstNumber * secondNumber)
+print("Division: ", firstNumber, "/", secondNumber, "=", firstNumber / secondNumber)
+print("Floor Division: ", firstNumber, "//", secondNumber, "=", firstNumber // secondNumber)
+print("Modulo: ", firstNumber, "%", secondNumber, "=", firstNumber % secondNumber)
+print("Power: ", firstNumber, "**", secondNumber, "=", firstNumber ** secondNumber)'''
+
+'''print("This application will calculate the perimeter of the square")
+side = int(input("Input the measurement of one side: "))
+answer = side * 4
+print("The perimeter of the square is: ", answer)
+
+print("This application will calculate the area of the square")
+side = int(input("Input the measurement of one side: "))
+answer = side ** 2
+print("The area of the square is: ", answer)
+
+print("This application will calculate the perimeter of the circle")
+radius = float(input("Input the measurement of the radius: "))
+answer = 2 * 3.14 * radius
+print("The perimeter of the circle is: ", answer)
+
+print("This application will calculate the area of the circle")
+side = float(input("Input the measurement of the radius: "))
+answer = 3.14 * (radius ** 2)
+print("The area of the circle is: ", answer)'''
+
+number1, number2, number3 = 4, 23, 922
+answer = number1 + number2 + number3
+print(f"The sum of the three numbers is {answer}")
